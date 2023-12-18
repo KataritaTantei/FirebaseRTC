91b124d236a195735d4b19753546e4d7aa458402
Taku Yamamoto <jts0ymt@outlook.jp>
2023-12-19 01:01:46 +0900

    チャットを動かすのに必要なファイルを追加

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -1,5 +1,10 @@
 import { build } from 'esbuild';
-import { copyFile } from 'node:fs';
+import { copyFile, mkdir } from 'node:fs';
+
+mkdir('public', (err) => {
+    if (err) throw err;
+    console.log('ディレクトリ作成完了: ./public')
+});
 
 build({
     entryPoints: ['src/index.js','src/chat.js'],
@@ -12,4 +17,14 @@ build({
 copyFile('src/index.html', 'public/index.html', (err) => {
     if (err) throw err;
     console.log('コピー完了: src/index.html to public/index.html');
+});
+
+copyFile('src/chat.html', 'public/chat.html', (err) => {
+    if (err) throw err;
+    console.log('コピー完了: src/chat.html to public/chat.html');
+});
+
+copyFile('src/chat.css', 'public/chat.css', (err) => {
+    if (err) throw err;
+    console.log('コピー完了: src/chat.css to public/chat.css');
 });
